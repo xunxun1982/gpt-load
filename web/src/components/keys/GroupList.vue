6a869faf6d592ca161bc55e1ae179ebf07050308
xunxun <xunxun1982@gmail.com>
2026-01-29 19:22:13 +0800

    fix: 修复GroupList组件中nextTick未导入导致的运行时错误
    修复了web/src/components/keys/GroupList.vue中nextTick函数未从Vue导入的问题。
    该函数在scrollToChannelType和scrollToSection方法中被使用，但缺少导入声明会导致运行时错误。
    
    变更内容：
    - 在Vue导入语句中添加nextTick函数
    - 确保快速导航功能正常工作

--- web/src/components/keys/GroupList.vue
+++ web/src/components/keys/GroupList.vue
@@ -31,7 +31,7 @@ import {
   useDialog,
   useMessage,
 } from "naive-ui";
-import { computed, ref, watch } from "vue";
+import { computed, nextTick, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import AggregateGroupModal from "./AggregateGroupModal.vue";
 import ChildGroupModal from "./ChildGroupModal.vue";
